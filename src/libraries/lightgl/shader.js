2835b4ed416c5093696105b2614e979a259533b3
Diego <sigma.octantis@gmai.com>
2017-06-02 09:08:13 -0400

    ifdef tweak 000

--- src/libraries/lightgl/shader.js
+++ src/libraries/lightgl/shader.js
@@ -65,7 +65,7 @@ export function Shader(vertexSource, fragmentSource) {
     }\
   ';
   var fragmentHeader = '\
-    #if GL_FRAGMENT_PRECISION_HIGH == 1 \n precision highp float;\n #else \n precision mediump float;\n #endif \n \
+    #ifdef GL_FRAGMENT_PRECISION_HIGH \n precision highp float;\n #else \n precision mediump float;\n #endif \n \
   ' + header;
 
   // change this depending on device compatibility between highp and mediump
